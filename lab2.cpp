b866cea31756ac75e8db3e0d1d1524aa2e58ba8e
Dason Baird <dbaird@odin.cs.csubak.edu>
2025-01-29 16:18:10 -0800

    3 $ 4

--- lab2.cpp
+++ lab2.cpp
@@ -1,7 +1,7 @@
-// This text is added during lab
+//
 //modified by: Dason B
 //date: Spring 2025
-// 
+//
 //original author: Gordon Griesel
 //date:            2025
 //purpose:         OpenGL sample program
@@ -26,30 +26,32 @@ using namespace std;
 
 class Global {
     public:
-	float w;
-	float dir;
-	float pos[2];
-	int red, blue, green;
-	int xres, yres;
-	Global();
+        float w;
+        float dir;
+        float ydir;
+        float pos[2];
+        float ypos[2];
+        int red, blue, green;
+        int xres, yres;
+        Global();
 } g;
 
 class X11_wrapper {
     private:
-	Display *dpy;
-	Window win;
-	GLXContext glc;
+        Display *dpy;
+        Window win;
+        GLXContext glc;
     public:
-	~X11_wrapper();
-	X11_wrapper();
-	void set_title();
-	bool getXPending();
-	XEvent getXNextEvent();
-	void swapBuffers();
-	void reshape_window(int width, int height);
-	void check_resize(XEvent *e);
-	void check_mouse(XEvent *e);
-	int check_keys(XEvent *e);
+        ~X11_wrapper();
+        X11_wrapper();
+        void set_title();
+        bool getXPending();
+        XEvent getXNextEvent();
+        void swapBuffers();
+        void reshape_window(int width, int height);
+        void check_resize(XEvent *e);
+        void check_mouse(XEvent *e);
+        int check_keys(XEvent *e);
 } x11;
 
 //Function prototypes
@@ -64,19 +66,17 @@ int main()
     int done = 0;
     //main game loop
     while (!done) {
-	//look for external events such as keyboard, mouse.
-	while (x11.getXPending()) {
-	    XEvent e = x11.getXNextEvent();
-	    x11.check_resize(&e);
-	    x11.check_mouse(&e);
-	    done = x11.check_keys(&e);
-	}
-	physics();
-	if (g.xres > g.w+20){
-	    render();
-	}
-	x11.swapBuffers();
-	usleep(200);
+        //look for external events such as keyboard, mouse.
+        while (x11.getXPending()) {
+            XEvent e = x11.getXNextEvent();
+            x11.check_resize(&e);
+            x11.check_mouse(&e);
+            done = x11.check_keys(&e);
+        }
+        physics();
+        render();
+        x11.swapBuffers();
+        usleep(200);
     }
     return 0;
 }
@@ -87,6 +87,7 @@ Global::Global()
     yres = 200;
     w = 20.0f;
     dir = 30.0f;
+    ydir = 30.0f;
     pos[0] = 0.0f+w;
     pos[1] = g.yres/2.0f;
     red = 100;
@@ -107,25 +108,25 @@ X11_wrapper::X11_wrapper()
     int w = g.xres, h = g.yres;
     dpy = XOpenDisplay(NULL);
     if (dpy == NULL) {
-	cout << "\n\tcannot connect to X server\n" << endl;
-	exit(EXIT_FAILURE);
+        cout << "\n\tcannot connect to X server\n" << endl;
+        exit(EXIT_FAILURE);
     }
     Window root = DefaultRootWindow(dpy);
     XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
     if (vi == NULL) {
-	cout << "\n\tno appropriate visual found\n" << endl;
-	exit(EXIT_FAILURE);
+        cout << "\n\tno appropriate visual found\n" << endl;
+        exit(EXIT_FAILURE);
     } 
     Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
     XSetWindowAttributes swa;
     swa.colormap = cmap;
     swa.event_mask =
-	ExposureMask | KeyPressMask | KeyReleaseMask |
-	ButtonPress | ButtonReleaseMask |
-	PointerMotionMask |
-	StructureNotifyMask | SubstructureNotifyMask;
+        ExposureMask | KeyPressMask | KeyReleaseMask |
+        ButtonPress | ButtonReleaseMask |
+        PointerMotionMask |
+        StructureNotifyMask | SubstructureNotifyMask;
     win = XCreateWindow(dpy, root, 0, 0, w, h, 0, vi->depth,
-	    InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
+            InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
     set_title();
     glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
     glXMakeCurrent(dpy, win, glc);
@@ -174,11 +175,11 @@ void X11_wrapper::check_resize(XEvent *e)
     //The ConfigureNotify is sent by the
     //server if the window is resized.
     if (e->type != ConfigureNotify)
-	return;
+        return;
     XConfigureEvent xce = e->xconfigure;
     if (xce.width != g.xres || xce.height != g.yres) {
-	//Window size did change.
-	reshape_window(xce.width, xce.height);
+        //Window size did change.
+        reshape_window(xce.width, xce.height);
     }
 }
 //-----------------------------------------------------------------------------
@@ -190,52 +191,79 @@ void X11_wrapper::check_mouse(XEvent *e)
 
     //Weed out non-mouse events
     if (e->type != ButtonRelease &&
-	    e->type != ButtonPress &&
-	    e->type != MotionNotify) {
-	//This is not a mouse event that we care about.
-	return;
+            e->type != ButtonPress &&
+            e->type != MotionNotify) {
+        //This is not a mouse event that we care about.
+        return;
     }
     //
     if (e->type == ButtonRelease) {
-	return;
+        return;
     }
     if (e->type == ButtonPress) {
-	if (e->xbutton.button==1) {
-	    //Left button was pressed.
-	    //int y = g.yres - e->xbutton.y;
-	    return;
-	}
-	if (e->xbutton.button==3) {
-	    //Right button was pressed.
-	    return;
-	}
+        if (e->xbutton.button==1) {
+            //Left button was pressed.
+            //int y = g.yres - e->xbutton.y;
+            return;
+        }
+        if (e->xbutton.button==3) {
+            //Right button was pressed.
+            return;
+        }
     }
     if (e->type == MotionNotify) {
-	//The mouse moved!
-	if (savex != e->xbutton.x || savey != e->xbutton.y) {
-	    savex = e->xbutton.x;
-	    savey = e->xbutton.y;
-	    //Code placed here will execute whenever the mouse moves.
+        //The mouse moved!
+        if (savex != e->xbutton.x || savey != e->xbutton.y) {
+            savex = e->xbutton.x;
+            savey = e->xbutton.y;
+            //Code placed here will execute whenever the mouse moves.
 
 
-	}
+        }
     }
 }
 
 int X11_wrapper::check_keys(XEvent *e)
 {
     if (e->type != KeyPress && e->type != KeyRelease)
-	return 0;
+        return 0;
     int key = XLookupKeysym(&e->xkey, 0);
     if (e->type == KeyPress) {
-	switch (key) {
-	    case XK_a:
-		//the 'a' key was pressed
-		break;
-	    case XK_Escape:
-		//Escape key was pressed
-		return 1;
-	}
+        switch (key) {
+            case XK_a:
+                //the 'a' key was pressed
+                break;
+            case XK_f:
+                //the 'f' key was pressed
+                if (g.dir > 0) {
+                    g.dir += 5.0;
+                } else {
+                    g.dir -= 5.0;
+                }
+                if (g.ydir < 0) {
+                    g.ydir -= 5.0;
+                } else {
+                    g.ydir += 5.0;
+                }
+                break;
+            case XK_s:
+                //the 's' key was pressed
+                if (g.dir < 0) {
+                    g.dir += 5.0;
+                } else {
+                    g.dir -= 5.0;
+                }
+                if (g.ydir < 0) {
+                    g.ydir += 5.0;
+                } else {
+                    g.ydir -= 5.0;
+                }
+                break;
+
+            case XK_Escape:
+                //Escape key was pressed
+                return 1;
+        }
     }
     return 0;
 }
@@ -256,34 +284,50 @@ void init_opengl(void)
 void physics()
 {
     if (g.xres < 40) {
-	g.w = 0;
-	return;
+        g.w = 0;
+        return;
     } else {
-	g.w = 20;
+        g.w = 20;
     }
 
-    if (g.red != 0) {
-	g.blue += 15;
-	g.red -= 15;
+    if (g.red != 0 ) {
+        g.blue += 15;
+        g.red -= 15;
     }
     // Move the box.
     g.pos[0] += g.dir;
+    g.pos[1] += g.ydir;
 
     // Collision detection.
+    if (g.pos[1] >= (g.yres-g.w)) {
+        g.pos[1] = (g.yres-g.w);
+        g.ydir = -g.ydir;
+        g.blue = 0;
+        g.green = 0;
+        g.red = 255;
+    }
+    if (g.pos[1] <= g.w) {
+        g.pos[1] = g.w;
+        g.ydir = -g.ydir;
+        g.blue = 0;
+        g.green = 0;
+        g.red = 255;
+    }
+
     if (g.pos[0] >= (g.xres-g.w)) {
-	g.pos[0] = (g.xres-g.w);
-	g.dir = -g.dir;
-	g.blue = 0;
-	g.green = 0;
-	g.red = 255;
+        g.pos[0] = (g.xres-g.w);
+        g.dir = -g.dir;
+        g.blue = 0;
+        g.green = 0;
+        g.red = 255;
 
     }
     if (g.pos[0] <= g.w) {
-	g.pos[0] = g.w;
-	g.dir = -g.dir;
-	g.blue = 0;
-	g.green = 0;
-	g.red = 255;
+        g.pos[0] = g.w;
+        g.dir = -g.dir;
+        g.blue = 0;
+        g.green = 0;
+        g.red = 255;
     }
 
 }
